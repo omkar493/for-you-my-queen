7b1452fd5e6b1210bdef9723a0f86b8984d0bad5
omkar493 <daschomu141@gmail.com>
2026-08-01 23:16:20 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,1 +1,12 @@
+const music = document.getElementById("bgMusic");
 
+// Start music after the user clicks Begin
+document.getElementById("beginBtn").addEventListener("click", () => {
+
+    music.volume = 0.4;
+
+    music.play().catch(err => {
+        console.log("Music couldn't start:", err);
+    });
+
+});
